12e2cd721ec31d279d4e49934ff3756c3c9f7c78
Nurul Islam <nurulbdpersonal@gmail.com>
2022-06-02 11:10:57 +0600

    final code

---
+++ components/Country.js
@@ -0,0 +1,16 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+
+export default function Country({country}) {
+  return (
+    <View>
+      <Text style={{fontSize:20}}>Country: {country.name.common}</Text>
+      <Image
+      source={{
+          uri: country.flags.png
+      }}
+      style={{width: 100, height: 100}}
+      ></Image>
+    </View>
+  )
+}
